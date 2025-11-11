c0bf3eafa160ac589ca2ab8433c1c92f06c5dc23
Petko Todorov <p.todorov1015@gmail.com>
2025-11-11 18:11:38 +0200

    Disables debug, allows all hosts, and formats requirements file.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x*f7xkt+h1=7)m9un^hmt1n7x)!jstu-rso531^6as4k$0rp@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
