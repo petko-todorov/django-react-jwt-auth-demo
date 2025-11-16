4a3504c9b18b44fa9a3af3e42afe22665cf73f5f
Petko Todorov <p.todorov1015@gmail.com>
2025-11-16 12:06:55 +0200

    Refactor URL configuration by removing commented code and organizing imports

--- backend/testjwt/urls.py
+++ backend/testjwt/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from testjwt.views import RegisterView, LogoutView
-
-from testjwt.views import CustomTokenObtainPairView
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from testjwt.views import RegisterView, LogoutView, CustomTokenObtainPairView
 
 urlpatterns = [
-    # path('api/', include([
     path('register/', RegisterView.as_view(), name='register'),
-    path('token/', CustomTokenObtainPairView.as_view(), name='login'),  # Login endpoint
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token refresh endpoint
-    path('logout/', LogoutView.as_view(), name='logout'),  # Logout endpoint
-    # ])),
+    path('token/', CustomTokenObtainPairView.as_view(),
+         name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
